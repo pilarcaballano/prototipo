801815597d33040b8b838c0bb39e36e1009522b3
Ismaelmtas <ismaelgmtr@gmail.com>
2021-07-09 08:19:54 +0200

    Estructura de nueva tabla

--- BBDD_prototipo/bbdd_prototipo_estructura.sql
+++ BBDD_prototipo/bbdd_prototipo_estructura.sql
@@ -17,3 +17,11 @@ CREATE TABLE bd_prototipo.Provincias(
 	des_provincia VARCHAR (50) NOT NULL)
 	COMMENT = 'Tabla donde se encuentran las provincias';
 	
+CREATE TABLE bd_prototipo.Notificacion_Correo_Electronico_Pendiente(
+	n_cod_notificacion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	n_cod_provincia INT NOT NULL,
+	c_cod_os VARCHAR(20) NOT NULL,
+	d_fecha_diligencia DATE NOT NULL,
+	c_cod_nif_empresa VARCHAR (20) NOT NULL,
+	des_correo_electronico VARCHAR (50))
+	COMMENT = 'Tabla que almacena las solicitudes de cambio de email pendientes de revisar.';
