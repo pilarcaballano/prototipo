d9d8952100ee3e95af4a5590f34481fe1a07a1d2
Ismaelmtas <ismaelgmtr@gmail.com>
2021-07-09 12:06:58 +0200

    Cambio nombre

---
+++ comunicacioncorreo/src/main/java/es/mtas/itss/comunicacioncorreo/ComunicacionCorreoApplication.java
@@ -0,0 +1,13 @@
+package es.mtas.itss.comunicacioncorreo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacionCorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicacionCorreoApplication.class, args);
+	}
+
+}
